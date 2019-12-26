a332c27871ef24de72a88967c27412dc6aa2c816
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-26 21:55:47 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Migration[5.2]
+     def change 
+         create_table :costume_stores do |t|
+         t.string :name
+         t.integer :location
+         t.integer :number of costumes, or "costume inventory"
+         t.string :number of employees
+         t.string :whether or not "it's still in business"
+         t.timestamp :opening time
+         t.timestamp :closing time
+     end
+  
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateCostumes < ActiveRecord::Migration[5.2]
+     def change 
+         create_table :haunted_houses do |t|
+         t.string :name
+         t.string :location
+         t.string :theme
+         t.integer :price
+         t.string :whether "they're family friendly or not"
+         t.timestamp :opening date
+         t.timestamp :closing date
+         t.string :long description
+     end
+end
