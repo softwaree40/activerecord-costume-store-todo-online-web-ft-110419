4753d223fae2406093504339fb2c4315205f26f9
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-26 22:07:05 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,9 +6,9 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
          t.string :location
          t.string :theme
          t.integer :price
-         t.string :whether "they're family friendly or not"
-         t.timestamp :opening date
-         t.timestamp :closing date
+         t.string :family_friendly
+         t.timestamp :opening_date
+         t.timestamp :closing_date
          t.string :long description
      end
 end
