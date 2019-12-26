7c3fa96fbb2888254b9fba982e35c79b2063c4f4
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-26 22:18:58 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,5 @@
 # Create your costume_stores migration here
-class CreateCostumeStore < ActiveRecord::Migration[5.2]
+class CostumeStore < ActiveRecord::Migration[5.2]
      def change 
          create_table :costume_stores do |t|
          t.string :name
