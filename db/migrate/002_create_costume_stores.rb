cd8637d9c11947d1431c116ca4de55ac9440e452
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-26 22:26:36 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+     def change 
+         create_table :costume_stores do |t|
+         t.string :name
+         t.integer :location
+         t.integer :costume_inventory
+         t.string :num_of_employees
+         t.string :still_in_business
+         t.timestamp :opening_time
+         t.timestamp :closing_time
+        end
+     end
+  
+end
